887ef027a5d19e5eebb90839b9fc77163af85f41
MirkoLorenzoLaRocca <mirkolorenzo.larocca@gmail.com>
2024-11-15 15:06:40 +0100

    update nnn

--- neural_network.py
+++ neural_network.py
@@ -6,8 +6,17 @@
 #import numpy as np
 import pandas as pd
 
+#path
 path = fr'tactigon\csvcomplete'
-df1, df2, df3, df4 = pd.DataFrame(path+fr'\registrazionidx_complete.csv', path+fr'\registrazionigiù_complete.csv'), 
-pd.DataFrame(path+fr'\registrazionisopra_complete.csv'), pd.DataFrame(path+fr'\registrazionisx_complete.csv')
+
+# da csv a df
+df1 = pd.read_csv(path + fr'\registrazionidx_complete.csv')
+df2 = pd.read_csv(path + fr'\registrazionigiù_complete.csv')
+df3 = pd.read_csv(path + fr'\registrazionisopra_complete.csv')
+df4 = pd.read_csv(path + fr'\registrazionisx_complete.csv')
+
+# Concatenazione dei DataFrame
 train_set = pd.concat([df1, df2, df3, df4])
+
+# Controllo concat
 print(train_set.head())
