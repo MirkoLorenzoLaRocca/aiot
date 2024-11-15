5197fe0b5915d1bbd6cf455241dd9695d9d330ab
MirkoLorenzoLaRocca <mirkolorenzo.larocca@gmail.com>
2024-11-15 14:45:06 +0100

    neural network file

---
+++ neural_network.py
@@ -0,0 +1,9 @@
+from keras.src.models import Sequential
+from keras.src.layers import Dense 
+from keras.src.optimizers import Adam
+from keras.src.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+train_set = pd.concat()
