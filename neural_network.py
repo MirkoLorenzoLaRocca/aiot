dd24aed6511804509c5c4bb10c4db797cf56aa48
MirkoLorenzoLaRocca <mirkolorenzo.larocca@gmail.com>
2024-11-15 14:54:12 +0100

    update neural network

--- neural_network.py
+++ neural_network.py
@@ -1,9 +1,13 @@
-from keras.src.models import Sequential
-from keras.src.layers import Dense 
-from keras.src.optimizers import Adam
-from keras.src.callbacks import EarlyStopping
-import matplotlib.pyplot as plt
-import numpy as np
+#from keras.src.models import Sequential
+#from keras.src.layers import Dense 
+#from keras.src.optimizers import Adam
+#from keras.src.callbacks import EarlyStopping
+#import matplotlib.pyplot as plt
+#import numpy as np
 import pandas as pd
 
-train_set = pd.concat()
+path = fr'tactigon\csvcomplete'
+df1, df2, df3, df4 = pd.DataFrame(path+fr'\registrazionidx_complete.csv', path+fr'\registrazionigiù_complete.csv'), 
+pd.DataFrame(path+fr'\registrazionisopra_complete.csv'), pd.DataFrame(path+fr'\registrazionisx_complete.csv')
+train_set = pd.concat([df1, df2, df3, df4])
+print(train_set.head())
